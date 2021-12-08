4d474486ba4211caa8bc0a663fad3031b987e9e3
lauramolez <lauramorelez@gmail.com>
2021-12-08 20:11:36 +0100

    recuperado objeto

--- spaPaises/src/app/pais_module/interfaces/countries.interface.ts
+++ spaPaises/src/app/pais_module/interfaces/countries.interface.ts
@@ -1,12 +1,167 @@
-
-//¡¡CÓDIGO CONSEGUIDO AL PEGAR EL RESULTADO OBTENIDO DE THUNDER CLIENT
-//(DATOS OBTENIDOS AL HACER LA CONSULTA QUE ESPECIFICA EN LA PÁGINA REST COUNTRIES: https://restcountries.com/v2/all?fields=name,population,flag)
-//EN LA PÁGINA QUICKTYPE!!
+// To parse this data:
+//
+//   import { Convert } from "./file";
+//
+//   const searchRESTCountries = Convert.toSearchRESTCountries(json);
+//
+// These functions will throw an error if the JSON doesn't
+// match the expected interface, even if the JSON is valid.
 
 export interface SearchRESTCountries {
-  flag:       string;
-  name:       string;
-  population: number;
+  name:         Name[];
+  tld:          string[];
+  cca2:         string;
+  ccn3:         string;
+  cca3:         string;
+  cioc?:        string;
+  independent:  boolean;
+  status:       Status;
+  unMember:     boolean;
+  currencies:   { [key: string]: Currency };
+  idd:          Idd;
+  capital?:     string[];
+  altSpellings: string[];
+  region:       Region;
+  subregion?:   string;
+  languages:    Languages;
+  translations: { [key: string]: Translation };
+  latlng:       number[];
+  landlocked:   boolean;
+  borders?:     string[];
+  area:         number;
+  demonyms:     Demonyms;
+  flag?:        string;
+  maps:         Maps;
+  population:   number;
+  gini?:        { [key: string]: number };
+  fifa?:        string;
+  car:          Car;
+  timezones:    string[];
+  continents:   string[];
+  flags:        CoatOfArms;
+  coatOfArms:   CoatOfArms;
+  startOfWeek:  StartOfWeek;
+  capitalInfo:  CapitalInfo;
+  postalCode?:  PostalCode;
+}
+
+export interface CapitalInfo {
+  latlng?: number[];
+}
+
+export interface Car {
+  signs: string[];
+  side:  Side;
+}
+
+export enum Side {
+  Left = "left",
+  Right = "right",
+}
+
+export interface CoatOfArms {
+  png?: string;
+  svg?: string;
+}
+
+export interface Currency {
+  name:   string;
+  symbol: string;
+}
+
+export interface Demonyms {
+  eng:  EngClass;
+  fra?: EngClass;
+}
+
+export interface EngClass {
+  f: string;
+  m: string;
+}
+
+export interface Idd {
+  root:     string;
+  suffixes: string[];
+}
+
+export interface Languages {
+  aym?: string;
+  grn?: string;
+  que?: string;
+  spa?: string;
+  kaz?: string;
+  rus?: string;
+  por?: string;
+  zho?: string;
+  fra?: string;
+  est?: string;
+  ara?: string;
+  deu?: string;
+  fas?: string;
+  eng?: EngEnum;
+  nfr?: string;
+  ssw?: string;
+  div?: string;
+  zdj?: string;
+  fil?: string;
+  aze?: string;
+  kir?: string;
+  ces?: string;
+  slk?: string;
+  crs?: string;
+  uzb?: string;
+  tet?: string;
+  lit?: string;
+  nld?: string;
+  pap?: string;
+  ind?: string;
+  sot?: string;
+  bar?: string;
+  ben?: string;
+}
+
+export enum EngEnum {
+  English = "English",
+}
+
+export interface Maps {
+  googleMaps:     string;
+  openStreetMaps: string;
+}
+
+export interface Name {
+  common:     string;
+  official:   string;
+  nativeName: { [key: string]: Translation };
+}
+
+export interface Translation {
+  official: string;
+  common:   string;
+}
+
+export interface PostalCode {
+  format: string;
+  regex:  string;
+}
+
+export enum Region {
+  Africa = "Africa",
+  Americas = "Americas",
+  Antarctic = "Antarctic",
+  Asia = "Asia",
+  Europe = "Europe",
+  Oceania = "Oceania",
+}
+
+export enum StartOfWeek {
+  Monday = "monday",
+  Sunday = "sunday",
+  Turday = "turday",
+}
+
+export enum Status {
+  OfficiallyAssigned = "officially-assigned",
 }
 
 // Converts JSON strings to/from your types
@@ -155,8 +310,142 @@ function r(name: string) {
 
 const typeMap: any = {
   "SearchRESTCountries": o([
-      { json: "flag", js: "flag", typ: "" },
-      { json: "name", js: "name", typ: "" },
+      { json: "name", js: "name", typ: r("Name") },
+      { json: "tld", js: "tld", typ: a("") },
+      { json: "cca2", js: "cca2", typ: "" },
+      { json: "ccn3", js: "ccn3", typ: "" },
+      { json: "cca3", js: "cca3", typ: "" },
+      { json: "cioc", js: "cioc", typ: u(undefined, "") },
+      { json: "independent", js: "independent", typ: true },
+      { json: "status", js: "status", typ: r("Status") },
+      { json: "unMember", js: "unMember", typ: true },
+      { json: "currencies", js: "currencies", typ: m(r("Currency")) },
+      { json: "idd", js: "idd", typ: r("Idd") },
+      { json: "capital", js: "capital", typ: u(undefined, a("")) },
+      { json: "altSpellings", js: "altSpellings", typ: a("") },
+      { json: "region", js: "region", typ: r("Region") },
+      { json: "subregion", js: "subregion", typ: u(undefined, "") },
+      { json: "languages", js: "languages", typ: r("Languages") },
+      { json: "translations", js: "translations", typ: m(r("Translation")) },
+      { json: "latlng", js: "latlng", typ: a(3.14) },
+      { json: "landlocked", js: "landlocked", typ: true },
+      { json: "borders", js: "borders", typ: u(undefined, a("")) },
+      { json: "area", js: "area", typ: 0 },
+      { json: "demonyms", js: "demonyms", typ: r("Demonyms") },
+      { json: "flag", js: "flag", typ: u(undefined, "") },
+      { json: "maps", js: "maps", typ: r("Maps") },
       { json: "population", js: "population", typ: 0 },
+      { json: "gini", js: "gini", typ: u(undefined, m(3.14)) },
+      { json: "fifa", js: "fifa", typ: u(undefined, "") },
+      { json: "car", js: "car", typ: r("Car") },
+      { json: "timezones", js: "timezones", typ: a("") },
+      { json: "continents", js: "continents", typ: a("") },
+      { json: "flags", js: "flags", typ: r("CoatOfArms") },
+      { json: "coatOfArms", js: "coatOfArms", typ: r("CoatOfArms") },
+      { json: "startOfWeek", js: "startOfWeek", typ: r("StartOfWeek") },
+      { json: "capitalInfo", js: "capitalInfo", typ: r("CapitalInfo") },
+      { json: "postalCode", js: "postalCode", typ: u(undefined, r("PostalCode")) },
   ], false),
+  "CapitalInfo": o([
+      { json: "latlng", js: "latlng", typ: u(undefined, a(3.14)) },
+  ], false),
+  "Car": o([
+      { json: "signs", js: "signs", typ: a("") },
+      { json: "side", js: "side", typ: r("Side") },
+  ], false),
+  "CoatOfArms": o([
+      { json: "png", js: "png", typ: u(undefined, "") },
+      { json: "svg", js: "svg", typ: u(undefined, "") },
+  ], false),
+  "Currency": o([
+      { json: "name", js: "name", typ: "" },
+      { json: "symbol", js: "symbol", typ: "" },
+  ], false),
+  "Demonyms": o([
+      { json: "eng", js: "eng", typ: r("EngClass") },
+      { json: "fra", js: "fra", typ: u(undefined, r("EngClass")) },
+  ], false),
+  "EngClass": o([
+      { json: "f", js: "f", typ: "" },
+      { json: "m", js: "m", typ: "" },
+  ], false),
+  "Idd": o([
+      { json: "root", js: "root", typ: "" },
+      { json: "suffixes", js: "suffixes", typ: a("") },
+  ], false),
+  "Languages": o([
+      { json: "aym", js: "aym", typ: u(undefined, "") },
+      { json: "grn", js: "grn", typ: u(undefined, "") },
+      { json: "que", js: "que", typ: u(undefined, "") },
+      { json: "spa", js: "spa", typ: u(undefined, "") },
+      { json: "kaz", js: "kaz", typ: u(undefined, "") },
+      { json: "rus", js: "rus", typ: u(undefined, "") },
+      { json: "por", js: "por", typ: u(undefined, "") },
+      { json: "zho", js: "zho", typ: u(undefined, "") },
+      { json: "fra", js: "fra", typ: u(undefined, "") },
+      { json: "est", js: "est", typ: u(undefined, "") },
+      { json: "ara", js: "ara", typ: u(undefined, "") },
+      { json: "deu", js: "deu", typ: u(undefined, "") },
+      { json: "fas", js: "fas", typ: u(undefined, "") },
+      { json: "eng", js: "eng", typ: u(undefined, r("EngEnum")) },
+      { json: "nfr", js: "nfr", typ: u(undefined, "") },
+      { json: "ssw", js: "ssw", typ: u(undefined, "") },
+      { json: "div", js: "div", typ: u(undefined, "") },
+      { json: "zdj", js: "zdj", typ: u(undefined, "") },
+      { json: "fil", js: "fil", typ: u(undefined, "") },
+      { json: "aze", js: "aze", typ: u(undefined, "") },
+      { json: "kir", js: "kir", typ: u(undefined, "") },
+      { json: "ces", js: "ces", typ: u(undefined, "") },
+      { json: "slk", js: "slk", typ: u(undefined, "") },
+      { json: "crs", js: "crs", typ: u(undefined, "") },
+      { json: "uzb", js: "uzb", typ: u(undefined, "") },
+      { json: "tet", js: "tet", typ: u(undefined, "") },
+      { json: "lit", js: "lit", typ: u(undefined, "") },
+      { json: "nld", js: "nld", typ: u(undefined, "") },
+      { json: "pap", js: "pap", typ: u(undefined, "") },
+      { json: "ind", js: "ind", typ: u(undefined, "") },
+      { json: "sot", js: "sot", typ: u(undefined, "") },
+      { json: "bar", js: "bar", typ: u(undefined, "") },
+      { json: "ben", js: "ben", typ: u(undefined, "") },
+  ], false),
+  "Maps": o([
+      { json: "googleMaps", js: "googleMaps", typ: "" },
+      { json: "openStreetMaps", js: "openStreetMaps", typ: "" },
+  ], false),
+  "Name": o([
+      { json: "common", js: "common", typ: "" },
+      { json: "official", js: "official", typ: "" },
+      { json: "nativeName", js: "nativeName", typ: m(r("Translation")) },
+  ], false),
+  "Translation": o([
+      { json: "official", js: "official", typ: "" },
+      { json: "common", js: "common", typ: "" },
+  ], false),
+  "PostalCode": o([
+      { json: "format", js: "format", typ: "" },
+      { json: "regex", js: "regex", typ: "" },
+  ], false),
+  "Side": [
+      "left",
+      "right",
+  ],
+  "EngEnum": [
+      "English",
+  ],
+  "Region": [
+      "Africa",
+      "Americas",
+      "Antarctic",
+      "Asia",
+      "Europe",
+      "Oceania",
+  ],
+  "StartOfWeek": [
+      "monday",
+      "sunday",
+      "turday",
+  ],
+  "Status": [
+      "officially-assigned",
+  ],
 };
--- spaPaises/src/app/pais_module/services/paises-service.service.ts
+++ spaPaises/src/app/pais_module/services/paises-service.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
+import { SearchRESTCountries, Name } from '../interfaces/countries.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -9,20 +10,19 @@ export class PaisesServiceService {
   constructor(private httpClient:HttpClient) { }
 
   //VARIABLES PARA LA PETICIÓN
-  api_key:string="";
-  url_base:string="https://restcountries.com/"; //v3.1 para datos y así para flags
-  resultado: string[] = [];
+  url_base:string="https://restcountries.com/alpha/"; //para que nos vaya sacando info de los caracteres que vayamos introduciendo
+  resultado: Name[] = [];
 
   buscarPaises(busqueda:string){//desde el componente por-pais le pasamos el nombre buscado y devolverá todo aquello que encaje con la búsqueda
 
     const params = new HttpParams()
-    .set('api_key', this.api_key)
     .set('q', busqueda);
 
-    this.httpClient.get<string>(this.url_base, {params:params}).subscribe(
+    //La url que he buscado en thunderClient es https://restcountries.com/v2/all.
+    this.httpClient.get<SearchRESTCountries>(this.url_base, {params:params}).subscribe(
       (resp) =>{
         console.log(resp);
-       // this.resultado = resp.data;
+       this.resultado = resp.name;
       }
     )
   }
