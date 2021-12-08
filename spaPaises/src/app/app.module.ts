57a35fd8078847e078572c37f01c265534eb1f89
lauramolez <lauramorelez@gmail.com>
2021-12-08 13:08:28 +0100

    estruturado

--- spaPaises/src/app/app.module.ts
+++ spaPaises/src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AppRoutingModule } from './app.routing';
 import { AppComponent } from './app.component';
+import { SharedModuleModule } from './shared-module/shared-module.module';
+import { SidebarComponent } from './shared-module/sidebar/sidebar.component';
+import { PaisModule } from './pais_module/pais.module';
+import { PorPaisComponent } from './pais_module/pages/por-pais/por-pais.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    PorPaisComponent
   ],
+
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    SharedModuleModule,
+    PaisModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ spaPaises/src/app/pais_module/pais.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PorCapitalComponent } from './pages/por-capital/por-capital.component';
+import { PorPaisComponent } from './pages/por-pais/por-pais.component';
+import { PorRegionComponent } from './pages/por-region/por-region.component';
+import { VerPaisComponent } from './pages/ver-pais/ver-pais.component';
+
+
+
+@NgModule({
+  declarations: [
+    PorCapitalComponent,
+    PorRegionComponent,
+    VerPaisComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class PaisModule { }
